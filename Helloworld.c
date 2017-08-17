9b19602b122ece72a4f88884b3f96479939ce619
BBoBLi <uss425@naver.com>
2017-08-17 23:45:13 +0900

    Create Helloworld.c
    hi

---
+++ Helloworld.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    while(1)
+      puts("HelloWorld!\n");
+}
+
+
+/*
+  #include <iostream>
+  
+  using namespace std;
+  
+  int main()
+  {
+       while(1)
+          cout<<"HelloWorld"<<endl;
+  }
+*/
