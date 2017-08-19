d8eb1b78ec423471800e49078aa2c7f8d1b80fa1
Phonedolly <c1john0803@gmail.com>
2017-08-19 15:58:34 +0900

    Add You class and Edit He.c

--- He.java
+++ He.java
@@ -1,6 +1,17 @@
-class He{
+public class He{
   public static void main(String[] args)
   {
     System.out.println("Hello!");
+    System.out.println("This is part of 1 DAY 1 COMMIT PROJECT");
+  }
+}
+
+public class You{
+
+  String name;
+  String tall;
+  public You(){
+    name = "NULL";
+    tall = "NULL";
   }
 }
--- Helloworld.c
+++ Helloworld.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
 int main()
-{
+  {
+    int foo;
     while(1)
+    {
       puts("HelloWorld!\n");
+
+      scanf_s(&foo, %d);
+      if(foo == 0)
+      {
+        printf_s("HELLO!");
+        break;
+      }
 }
 
 
